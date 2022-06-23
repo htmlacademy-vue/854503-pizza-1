d7ef890feba3ac6014efd1b77753b9c16d854121
FIROKS <firoks1@gmail.com>
2022-06-23 17:46:24 +0300

    Убирает лишнее правило валидатора

--- src/frontend/src/common/mixins/validator.js
+++ src/frontend/src/common/mixins/validator.js
@@ -1,10 +1,6 @@
 import { EMAIL_REGEX, TEL_REGEX } from "@/common/const";
 
 const rules = {
-  isNotEmpty: {
-    rule: (value) => !!value?.trim(),
-    message: "Поле не заполнено",
-  },
   required: {
     rule: (value) => !!value?.trim(),
     message: "Поле обязательно для заполнения",
